1d406e489155e5c2e5a6d2aea13a343dc14e18ad
maurosampietro <mauro.sampietro@gmail.com>
2017-03-10 15:22:05 +0100

    Conventional mapping tests

---
+++ TypeMapper.Tests/MappingConventionTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeMapper.Tests
+{
+    [TestClass]
+    public class MappingConventionTests
+    {
+        [TestMethod]
+        public void EnableConventionsGlobally()
+        {
+
+        }
+
+        [TestMethod]
+        public void EnableConventionsGloballyButDisableOnSpecificType()
+        {
+
+        }
+
+        [TestMethod]
+        public void DisableConventionsGlobally()
+        {
+
+        }
+
+        [TestMethod]
+        public void DisableConventionsGloballyButEnableOnSpecificType()
+        {
+
+        }
+    }
+}
