e88d397d5d656412c17719845eae8bfca2133cfa
hermanlindner <hermanlindner@gmail.com>
2023-03-31 15:39:47 +0200

    This is the test class which lays bare the projection bug that I have been facing. I do not have a PR for fixing this.
    What I saw is that the label thing is used 2 times in the method and that htat is what this is about.
    It seems to have something to do that in logging we want to show the value that is not mapping right.
    
    A concern with that is that other people could face with that implementation is that when the mapped value happens to be a secret value that
    should never be in the log files.  That will be in the log files when the secret is not being able to map.
    Dat is a security concern with getting values in the exceptions.

---
+++ UltraMapper.Tests/RealworldBugs/RealworldBug3.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UltraMapper.Tests.RealworldBugs
+{
+    [TestClass]
+    public class RealworldBug3
+    {
+
+        public class A
+        { 
+            public B BProp { get; set; }
+            public C CProp { get; set; }
+        }
+
+        public class B
+        {
+            public long Id { get; set; }
+        }
+
+        public class C
+        {
+            public long Id { get; set; }
+        }
+
+        public class D
+        {
+            public long? B_Id { get; set; }
+            public long? C_Id { get; set; }
+        }
+
+
+        [TestMethod]
+        public void StringToGuidTypeMapping()
+        {
+            var mapper = new Mapper( cfg =>
+            {
+                cfg.MapTypes<A, D>()
+                 .MapMember( a => a.BProp.Id, d => d.B_Id )
+                 .MapMember( a => a.CProp.Id, d => d.C_Id );
+            } );
+
+            var aObject = new A
+            {
+                BProp = new B { Id = 1 },
+                CProp = new C { Id = 2 }
+            };
+
+            var result = mapper.Map<D>( aObject);
+
+            Assert.AreEqual( result.B_Id, 1L );
+            Assert.AreEqual( result.C_Id, 2L );
+        }
+    }
+}
