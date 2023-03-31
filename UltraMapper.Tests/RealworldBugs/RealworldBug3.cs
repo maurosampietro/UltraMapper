a16d3b3e0549e99f117f1336a24b5b03193bb2c0
hermanlindner <hermanlindner@gmail.com>
2023-03-31 16:44:33 +0200

    No also have the null value in the test and give it a proper test name.

--- UltraMapper.Tests/RealworldBugs/RealworldBug3.cs
+++ UltraMapper.Tests/RealworldBugs/RealworldBug3.cs
@@ -30,7 +30,7 @@ namespace UltraMapper.Tests.RealworldBugs
 
 
         [TestMethod]
-        public void StringToGuidTypeMapping()
+        public void ProjectionMappingFull()
         {
             var mapper = new Mapper( cfg =>
             {
@@ -49,6 +49,28 @@ namespace UltraMapper.Tests.RealworldBugs
 
             Assert.AreEqual( result.B_Id, 1L );
             Assert.AreEqual( result.C_Id, 2L );
+        }
+
+        [TestMethod]
+        public void ProjectionMappingNull()
+        {
+            var mapper = new Mapper( cfg =>
+            {
+                cfg.MapTypes<A, D>()
+                 .MapMember( a => a.BProp.Id, d => d.B_Id )
+                 .MapMember( a => a.CProp.Id, d => d.C_Id );
+            } );
+
+            var aObject = new A
+            {
+                BProp = null,
+                CProp = null
+            };
+
+            var result = mapper.Map<D>( aObject );
+
+            Assert.IsNull( result.B_Id );
+            Assert.IsNull( result.C_Id );
         }
     }
 }
