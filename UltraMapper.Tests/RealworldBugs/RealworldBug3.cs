88483292d60194a2c97ad2f738bfa7c456ed0345
hermanlindner <hermanlindner@gmail.com>
2023-04-16 13:55:19 +0200

    adding extra test classes for each projection maping scenario.

--- UltraMapper.Tests/RealworldBugs/RealworldBug3.cs
+++ UltraMapper.Tests/RealworldBugs/RealworldBug3.cs
@@ -7,7 +7,7 @@ namespace UltraMapper.Tests.RealworldBugs
     {
 
         public class A
-        { 
+        {
             public B BProp { get; set; }
             public C CProp { get; set; }
         }
@@ -28,24 +28,9 @@ namespace UltraMapper.Tests.RealworldBugs
             public long? C_Id { get; set; }
         }
 
-        public class E
-        {
-            public long B_Id { get; set; }
-            public long C_Id { get; set; }
-        }
-
-        public class F
-        {
-            public A GetA()
-            {
-                return AProp;
-            }
-
-            public A AProp { get; set; }
-        }
 
         [TestMethod]
-        public void ProjectionMappingFullToNullabe()
+        public void ProjectionMappingFull()
         {
             var mapper = new Mapper( cfg =>
             {
@@ -60,37 +45,14 @@ namespace UltraMapper.Tests.RealworldBugs
                 CProp = new C { Id = 2 }
             };
 
-            var result = mapper.Map<D>( aObject);
+            var result = mapper.Map<D>( aObject );
 
             Assert.AreEqual( result.B_Id, 1L );
             Assert.AreEqual( result.C_Id, 2L );
         }
 
         [TestMethod]
-        public void ProjectionMappingFullToNullabeMethod()
-        {
-            var mapper = new Mapper( cfg =>
-            {
-                cfg.MapTypes<F, D>()
-                 .MapMember( f => f.GetA().BProp.Id, d => d.B_Id )
-                 .MapMember( f => f.GetA().CProp.Id, d => d.C_Id );
-            } );
-
-            var aObject = new A
-            {
-                BProp = new B { Id = 1 },
-                CProp = new C { Id = 2 }
-            };
-            var fObject = new F { AProp = aObject };
-            var result = mapper.Map<D>( fObject );
-            Assert.AreEqual( result.B_Id, 1L );
-            Assert.AreEqual( result.C_Id, 2L );
-        }
-
-
-
-        [TestMethod]
-        public void ProjectionMappingNullToNullable()
+        public void ProjectionMappingNull()
         {
             var mapper = new Mapper( cfg =>
             {
@@ -110,111 +72,5 @@ namespace UltraMapper.Tests.RealworldBugs
             Assert.IsNull( result.B_Id );
             Assert.IsNull( result.C_Id );
         }
-
-        [TestMethod]
-        public void ProjectionMappingNullToNullableMethod()
-        {
-            var mapper = new Mapper( cfg =>
-            {
-                cfg.MapTypes<F, D>()
-                 .MapMember( f => f.GetA().BProp.Id, d => d.B_Id )
-                 .MapMember( f => f.GetA().CProp.Id, d => d.C_Id );
-            } );
-            var fObject = new F();
-            var result = mapper.Map<D>( fObject );
-            Assert.IsNull( result.B_Id );
-            Assert.IsNull( result.C_Id );
-        }
-
-
-        [TestMethod]
-        public void ProjectionMappingFull()
-        {
-            var mapper = new Mapper( cfg =>
-            {
-                cfg.MapTypes<A, E>()
-                 .MapMember( a => a.BProp.Id, d => d.B_Id )
-                 .MapMember( a => a.CProp.Id, d => d.C_Id );
-            } );
-
-            var aObject = new A
-            {
-                BProp = new B { Id = 1 },
-                CProp = new C { Id = 2 }
-            };
-
-            var result = mapper.Map<E>( aObject );
-
-            Assert.AreEqual( result.B_Id, 1L );
-            Assert.AreEqual( result.C_Id, 2L );
-        }
-
-        [TestMethod]
-        public void ProjectionMappingDefault()
-        {
-            var mapper = new Mapper( cfg =>
-            {
-                cfg.MapTypes<A, E>()
-                 .MapMember( a => a.BProp.Id, d => d.B_Id )
-                 .MapMember( a => a.CProp.Id, d => d.C_Id );
-            } );
-
-            var aObject = new A
-            {
-                BProp = null,
-                CProp = null
-            };
-
-            var result = mapper.Map<E>( aObject );
-
-            Assert.AreEqual( 0, result.B_Id );
-            Assert.AreEqual( 0, result.C_Id );
-        }
-
-        [TestMethod]
-        public void ProjectionMappingFullMethod()
-        {
-            var mapper = new Mapper( cfg =>
-            {
-                cfg.MapTypes<F, E>()
-                 .MapMember( a => a.GetA().BProp.Id, d => d.B_Id )
-                 .MapMember( a => a.GetA().CProp.Id, d => d.C_Id );
-            } );
-
-            var aObject = new A
-            {
-                BProp = new B { Id = 1 },
-                CProp = new C { Id = 2 }
-            };
-            var fObject = new F { AProp = aObject };
-            var result = mapper.Map<E>( fObject );
-
-            Assert.AreEqual( result.B_Id, 1L );
-            Assert.AreEqual( result.C_Id, 2L );
-        }
-
-        [TestMethod]
-        public void ProjectionMappingDefaultMethod()
-        {
-            var mapper = new Mapper( cfg =>
-            {
-                cfg.MapTypes<F, E>()
-                 .MapMember( a => a.GetA().BProp.Id, d => d.B_Id )
-                 .MapMember( a => a.GetA().CProp.Id, d => d.C_Id );
-            } );
-
-            var aObject = new A
-            {
-                BProp = null,
-                CProp = null
-            };
-            var fObject = new F { AProp = aObject };
-            var result = mapper.Map<E>( fObject );
-
-            Assert.AreEqual( 0, result.B_Id );
-            Assert.AreEqual( 0, result.C_Id );
-        }
-
-
     }
 }
